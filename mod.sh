76a213394c131443aacfeff313f273eabd4e38ba
qpzr <58745620+qpzr@users.noreply.github.com>
2023-05-11 01:55:42 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -11,6 +11,8 @@ sed -i "s/ntp1.aliyun.com/cn.ntp.org.cn/g" ./user/shared/defaults.h
 echo '修改NTP2为国家授时中心'
 sed -i "s/2001:470:0:50::2/ntp.ntsc.ac.cn/g" ./user/shared/defaults.h
 
+svn co https://github.com/hanwckf/padavan-4.4/branches/main/trunk/build_firmware_modify
+
 echo '超级精简配置'
 sed -i '/### Enable USB support/,$d' ./configs/templates/*.config
 sed -i '$a\CONFIG_FIRMWARE_INCLUDE_LANG_CN=y'  ./configs/templates/*.config
